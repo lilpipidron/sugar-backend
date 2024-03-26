23a5b23b8da25cf7f808b1f8b93f2ed1c74e1141
zzucch <nikitamilovatskiy@gmail.com>
2024-03-27 00:25:04 +0300

    feat: add custom storage errors

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,5 +1,21 @@
 package storage
 
-var(
-//errors here
+import "errors"
+
+var (
+	ErrUserNotFound    = errors.New("user not found")
+	ErrNoteNotFound    = errors.New("note not found")
+	ErrProductNotFound = errors.New("product not found")
+
+	ErrInvalidName              = errors.New("invalid name")
+	ErrInvalidDate              = errors.New("invalid date")
+	ErrInvalidWeight            = errors.New("invalid weight")
+	ErrInvalidSugarLevel        = errors.New("invalid sugar level")
+	ErrInvalidCarbsValue        = errors.New("invalid carbs value")
+	ErrInvalidBreadUnitsValue   = errors.New("invalid bread units value")
+	ErrInvalidCarbohydrateRatio = errors.New("invalid carbohydrate ratio")
+
+	ErrUserExists    = errors.New("user already exists")
+	ErrNoteExists    = errors.New("note already exists")
+	ErrProductExists = errors.New("product already exists")
 )

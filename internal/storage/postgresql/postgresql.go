3f6d8e94654b656ca5753c4da9ae4470ac65f7a9
Vadim <114152213+lilpipidron@users.noreply.github.com>
2024-03-27 14:37:23 +0300

    Revert "Feat/storage errors"

--- internal/storage/postgresql/postgresql.go
+++ internal/storage/postgresql/postgresql.go
@@ -3,7 +3,6 @@ package postgresql
 import (
 	"database/sql"
 	"errors"
-	"fmt"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
@@ -14,26 +13,26 @@ type Storage struct {
 }
 
 func New(psqlInfo, dbname string) (*Storage, error) {
-	const errFunc = "storage.postgresql.NewStorage"
+  const errFunc = "storage.postgresql.NewStorage"
 
-	db, err := sql.Open("postgres", psqlInfo)
+  db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		return nil, fmt.Errorf("%s: %w", errFunc, err)
+		return nil,  fmt.Errorf("%s: %w", errFunc, err)
 	}
 
 	driver, err := postgres.WithInstance(db, &postgres.Config{})
 	if err != nil {
-		return nil, fmt.Errorf("%s: %w", errFunc, err)
+		return nil,  fmt.Errorf("%s: %w", errFunc, err)
 	}
 
 	migration, err := migrate.NewWithDatabaseInstance("file:/migration", dbname, driver)
 	if err != nil {
-		return nil, fmt.Errorf("%s: %w", errFunc, err)
+		return nil,  fmt.Errorf("%s: %w", errFunc, err)
 	}
 
 	if err := migration.Up(); err != nil {
 		if !errors.Is(err, migrate.ErrNoChange) {
-			return nil, fmt.Errorf("%s: %w", errFunc, err)
+      return nil,  fmt.Errorf("%s: %w", errFunc, err)
 		}
 	}
 

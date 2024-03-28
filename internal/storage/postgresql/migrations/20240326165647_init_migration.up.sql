97edcde47dca06f8a20b47c3c3919875612754bf
lilpipidron <lociX55Lim@gmail.com>
2024-03-28 22:42:13 +0300

    feat: rewrite init up migration

--- internal/storage/postgresql/migrations/20240326165647_init_migration.up.sql
+++ internal/storage/postgresql/migrations/20240326165647_init_migration.up.sql
@@ -1,53 +1,59 @@
 create type note_types as enum
-(
+    (
         'regular'
-        'sugar'
-);
+            'sugar'
+        );
 
 create type genders as enum
-(
+    (
         'male'
-        'female'
-);
+            'female'
+        );
 
 create table users
 (
-        user_id bigint primary key generated always as identity,
-        login text not null,
-        password text not null
+    user_id  bigint primary key generated always as identity,
+    login    text not null,
+    password text not null
 );
 
 create table user_info
 (
-        user_id bigint not null references users(user_id),
-        name text not null,
-        birthday date not null,
-        gender genders not null,
-        weight bigint not null,
-        height bigint not null,
-        carbohydrate_ratio real not null,
-        bread_unit bigint not null
+    user_id            bigint  not null references users (user_id),
+    name               text    not null,
+    birthday           date    not null,
+    gender             genders not null,
+    weight             bigint  not null,
+    height             bigint  not null,
+    carbohydrate_ratio real    not null,
+    bread_unit         bigint  not null
 );
 
 create table product
 (
-        product_id bigint primary key generated always as identity,
-        product_name text not null,
-        carbs bigint not null
+    product_id   bigint primary key generated always as identity,
+    product_name text   not null,
+    carbs        bigint not null
+);
+
+
+create table note_user
+(
+    note_id bigint not null references note_header (note_id),
+    user_id bigint not null references users (user_id)
 );
 
 create table note_header
 (
-        note_id bigint primary key generated always as identity,
-        user_id bigint not null references users(user_id),
-        note_type note_types not null,
-        create_date date not null,
-        sugar_level real
+    note_id     bigint primary key generated always as identity,
+    note_type   note_types not null,
+    create_date date       not null,
+    sugar_level real
 );
 
 create table note_detail
 (
-        note_id bigint not null references note_header(note_id),
-        product_id bigint not null references product(product_id),
-        product_amount bigint not null
+    note_id        bigint not null references note_header (note_id),
+    product_id     bigint not null references product (product_id),
+    product_amount bigint not null
 );

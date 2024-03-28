53e3827c873054594041cfa37a582ff72a761027
lilpipidron <lociX55Lim@gmail.com>
2024-03-28 23:29:56 +0300

    refactor: add birthday for user

--- internal/models/users/info.go
+++ internal/models/users/info.go
@@ -1,9 +1,12 @@
 package users
 
+import "time"
+
 type UserInfo struct {
-	Name              string `json:"name"`
-	Gender            Gender `json:"gender"`
-	Weight            int    `json:"weight"`
-	CarbohydrateRatio int    `json:"carbohydrate-ratio"`
-	GrainUnit         int    `json:"grain-unit"`
+	Name              string    `json:"name"`
+	Birhday           time.Time `json:"birthday"`
+	Gender            Gender    `json:"gender"`
+	Weight            int       `json:"weight"`
+	CarbohydrateRatio int       `json:"carbohydrate-ratio"`
+	GrainUnit         int       `json:"grain-unit"`
 }

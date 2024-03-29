4226e4e3ed09b14747acc26a4b5f6f805761478a
zzucch <nikitamilovatskiy@gmail.com>
2024-03-29 20:29:29 +0300

    feat: add initial down migration

---
+++ internal/storage/postgresql/migrations/20240326165647_init_migration.down.sql
@@ -0,0 +1,15 @@
+drop type note_types cascade;
+
+drop type genders cascade;
+
+drop table users cascade;
+
+drop table user_info cascade;
+
+drop table products cascade;
+
+drop table note_user cascade;
+
+drop table note_header cascade;
+
+drop table note_detail cascade;

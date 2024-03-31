dc01f39ae4b935ab2328338d0b596db301cb57d5
lilpipidron <lociX55Lim@gmail.com>
2024-03-31 19:41:25 +0300

    feat: add func add new user

--- go.mod
+++ go.mod
@@ -3,24 +3,26 @@ module github.com/lilpipidron/sugar-backend
 go 1.22.1
 
 require (
+	github.com/go-chi/chi/v5 v5.0.12
+	github.com/go-chi/render v1.0.3
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/joho/godotenv v1.5.1
 )
 
 require (
-	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/ajg/form v1.5.1 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
 )
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/lipgloss v0.10.0 // indirect
-	github.com/charmbracelet/log v0.4.0 // indirect
-	github.com/go-chi/chi/v5 v5.0.12
+	github.com/charmbracelet/log v0.4.0
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/golang-migrate/migrate/v4 v4.17.0
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
@@ -30,7 +32,7 @@ require (
 	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
---
+++ internal/http-server/handlers/user/add.go
@@ -0,0 +1,94 @@
+package user
+
+import (
+	"errors"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/go-playground/validator"
+
+	"github.com/charmbracelet/log"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	resp "github.com/lilpipidron/sugar-backend/internal/lib/api/response"
+	"github.com/lilpipidron/sugar-backend/internal/models/users"
+)
+
+type Request struct {
+	Login             string       `json:"login"`
+	Password          string       `json:"password"`
+	Name              string       `json:"name"`
+	Birthday          time.Time    `json:"birthday"`
+	Gender            users.Gender `json:"gender"`
+	Weight            int          `json:"weight"`
+	CarbohydrateRatio int          `json:carbohydrate-ratio"`
+	BreadUnit         int          `json:"bread-unit`
+}
+
+type Response struct {
+	resp.Response
+}
+type UserSaver interface {
+	AddUser(user users.User, password string) error
+}
+
+func New(logger *log.Logger, userSaver UserSaver) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.user.add.New"
+		logger = log.With(
+			"op: "+op,
+			"request_id: "+middleware.GetReqID(r.Context()),
+		)
+
+		var req Request
+
+		err := render.DecodeJSON(r.Body, &req)
+		if errors.Is(err, io.EOF) {
+			log.Errorf("request body is empty")
+
+			render.JSON(w, r, resp.Error("empty request"))
+
+			return
+		}
+
+		if err != nil {
+			log.Error("failed to decode request body", err)
+
+			render.JSON(w, r, resp.Error("failed to decode request"))
+
+			return
+		}
+
+		log.Info("request body decoded", req)
+
+		if err != validator.New().Struct(req); err != nil {
+			validateErr := err.(validator.ValidationErrors)
+
+			log.Error("invalid request", err)
+
+			render.JSON(w, r, resp.Error(validateErr.Error()))
+
+			return
+		}
+		// FIX
+		err = userSaver.AddUser(req, req.Password)
+		if err != nil {
+			log.Error(err)
+
+			render.JSON(w, r, resp.Error("failed add user"))
+
+			return
+		}
+
+		log.Info("user added")
+
+		responseOK(w, r)
+	}
+}
+
+func responseOK(w http.ResponseWriter, r *http.Request) {
+	render.JSON(w, r, Response{
+		Response: resp.OK(),
+	})
+}

d64beb6efd512340b917c063ad86b4f3f120e8e4
lilpipidron <lociX55Lim@gmail.com>
2024-05-14 21:32:27 +0300

    fix: data unmarshal

--- internal/httpserver/request/changebirthday.go
+++ internal/httpserver/request/changebirthday.go
@@ -1,8 +1,27 @@
 package request
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 type ChangeBirthday struct {
 	ID          int64     `json:"id"`
 	NewBirthday time.Time `json:"new_birthday"`
+}
+
+func (u *ChangeBirthday) UnmarshalJSON(data []byte) error {
+	type Alias ChangeBirthday
+	aux := &struct {
+		NewBirthday string `json:"new_birthday"`
+		*Alias
+	}{
+		Alias: (*Alias)(u),
+	}
+	err := json.Unmarshal(data, &aux)
+	if err != nil {
+		return err
+	}
+	u.NewBirthday, err = time.Parse("2006-01-02", aux.NewBirthday)
+	return err
 }

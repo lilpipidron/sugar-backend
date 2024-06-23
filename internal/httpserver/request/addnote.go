7710da1402e1ae23c0beee5aeaf656427d5aa778
vadim <lociX55Lim@gmail.com>
2024-06-23 17:50:56 +0300

    feat: fix add note struct

--- internal/httpserver/request/addnote.go
+++ internal/httpserver/request/addnote.go
@@ -7,7 +7,7 @@ import (
 
 type AddNote struct {
 	UserID     int64          `json:"user-id"`
-	NoteID     int64          `json:"note-id"`
+	NoteID     int64          `json:"note-id" default:"0"`
 	NoteType   notes.NoteType `json:"note-type"`
 	DateTime   time.Time      `json:"date-time"`
 	SugarLevel int            `json:"sugar-level"`
